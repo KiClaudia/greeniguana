6efa5192e0f4f759f14dd1f8e9d847c9e9facad7
claudia ki <claudia.ki@usu.edu>
2021-11-24 14:28:42 -0700

    new scripts

--- code/2wayAOVbka.R
+++ code/2wayAOVbka.R
@@ -5,28 +5,69 @@ gi <- read.csv("C:/Users/claud/OneDrive - USU/Desktop/ASU green iguana 2021/gree
 View(gi)
 install.packages("tidyverse")
 install.packages("rstatix")
+install.packages("ggpubr")
 library("rstatix")
 library("tidyverse")
 library("dplyr")
+library("ggpubr")
 
 gi$diet <- as.factor(gi$diet)
 gi$lps <- as.factor(gi$lps)
+gi$time <- as.factor(gi$time)
+gi$iguanaID <- as.factor(gi$iguanaID)
+str(gi)
 
 GL <- gi %>%
-  filter(tx == "G-L") 
-gi %>%
-  group_by(tx) %>%
-  summarise(
-    mean(X0430bka, na.rm = TRUE)
+  filter(tx == "G-L") #filter the treatment groups out so we can look at summary stats
+GC <- gi %>%
+  filter(tx == "G-C")
+WL<- gi %>%
+  filter(tx == "W-L")
+WC<- gi %>%
+  filter(tx == "W-C")
+
+GL %>%
+  group_by(time) %>%
+  summarise(mean(bka, na.rm = TRUE)
   )
-boxplot(GL$X0423bka, GL$X0428bka, GL$X0430bka, GL$X0504bka, GL$X0511bka, GL$X0525bka,
-        main = "Progression of BKA after first LPS challenge for Glucose-LPS group",
-        ylab = "Bacterial Killing",
-        xlab = "Time Progression",
-        names = c("Pre-LPS", "24hr Post-LPS", "72hr", "1 week", "2 week", "4week"))
-res.aov <- anova_test(data = GL, dv = score, wid = id, within = time)
-get_anova_table(res.aov)
+GC %>%
+  group_by(time) %>%
+  summarise(mean(bka, na.rm = TRUE)
+  )
+WL %>%
+  group_by(time) %>%
+  summarise(mean(bka, na.rm = TRUE)
+  )
+WC %>%
+  group_by(time) %>%
+  summarise(mean(bka, na.rm = TRUE)
+  )
+
+boxplot(GL$bka ~ GL$time) # visualize the data
+boxplot(GC$bka ~ GC$time)
+boxplot(WL$bka ~ WL$time)
+boxplot(WC$bka ~ WC$time)
+
+GLaov <- anova_test(data = GL, dv = bka, wid = iguanaID, within = time)
+get_anova_table(GLaov) # difference across time but which pairs?
+
+GLpwc <- GL %>% # pair-wise comparison
+  pairwise_t_test(
+    bka ~ time, paired = FALSE,
+    p.adjust.method = "bonferroni"
+  )
+GLpwc
+
+#2 way aov (above is one way)
+gi %>%
+  group_by(tx, time) %>%
+  get_summary_stats(bka, type = "mean_sd") %>%
+  View()
+
+bxp <- ggboxplot(
+  gi, x = "time", y = "bka",
+  color = "tx", palette = "jco"
+)
+bxp
 
 
-
-     
---
+++ code/MMaovBKA.R
@@ -0,0 +1,8 @@
+# Mixed model anova or between-within anova is used to compare across groups of different
+# factors and within group (across time)
+
+# assumptions: 1) no outliers 2) normality 3) homogeneity of variances 4) sphericity
+# 5) homogenity of covariances
+# https://www.datanovia.com/en/lessons/mixed-anova-in-r/
+
+# we are going to look at interactions between treatment and time
